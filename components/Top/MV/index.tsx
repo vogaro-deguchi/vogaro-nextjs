cbb4ec95b190ded57f8e4a6cbb273b76230c7d1a
deguchi <deguchi37458@gmail.com>
2022-10-13 10:34:48 +0900

    create MV

---
+++ components/Top/MV/index.tsx
@@ -0,0 +1,13 @@
+import Swiper from '@/components/Top/Swiper'
+
+import styles from './index.module.scss'
+
+export default function MV(): JSX.Element {
+  return (
+    <>
+      <div className={styles.mvSec}>
+        <Swiper />
+      </div>
+    </>
+  )
+}
