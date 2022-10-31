4328ba7313d565cd0cb7680bd4c127c96cba4b01
deguchi <deguchi37458@gmail.com>
2022-10-31 16:04:52 +0900

    update swiper

--- components/Top/Swiper/index.tsx
+++ components/Top/Swiper/index.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react' //カルーセル用のタグをインポート
 import SwiperCore, { Pagination, Navigation } from 'swiper' //使いたい機能をインポート
 
+import styles from './index.module.scss'
+
 SwiperCore.use([Pagination, Navigation]) 
 
 // カルーセルにする画像のソースをリストにします
@@ -11,23 +13,28 @@ const images = [
   '/assets/Top/Swiper/mv_img03_md.jpg'
 ]
 
-const TestCarousel = () => {
+export default function SwiperComponent():JSX.Element {
   const prevRef = useRef<HTMLDivElement>(null)
   const nextRef = useRef<HTMLDivElement>(null)
   return (
-    <Swiper
-      slidesPerView={1.1}
-      pagination={{
-        clickable: true,
-      }}
-      navigation
-      loop={true}
-      centeredSlides={true}
-      onInit={(swiper) => {
-        swiper.navigation.init();
-        swiper.navigation.update();
-      }}
-    >
+    <div className={styles.swiperWrapper}>
+      <Swiper
+        slidesPerView={1.1}
+        pagination={{
+          el: "#pagination",
+          clickable: true,
+        }}
+        navigation={{
+          prevEl: "#button_prev",
+          nextEl: "#button_next"
+        }}
+        loop={true}
+        centeredSlides={true}
+        onInit={(swiper) => {
+          swiper.navigation.init();
+          swiper.navigation.update();
+        }}
+      >
       {images.map((src: string, index: number) => {
         return (
           <SwiperSlide key={`${index}`}>
@@ -42,8 +49,15 @@ const TestCarousel = () => {
           </SwiperSlide>
         )
       })}
-    </Swiper>
+      </Swiper>
+      <div id="button_prev" className={styles.swiperBtnPrev}></div>
+      <div id="button_next" className={styles.swiperBtnNext}></div>
+
+      <div id="pagination" className={styles.pagination}></div>
+    </div>
   )
 }
 
-export default TestCarousel
+<style>
+
+</style>
