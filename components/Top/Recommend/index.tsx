3c6e7ce1744ba8bf1b3fa2193cd745ac2de5dc7a
deguchi <deguchi37458@gmail.com>
2022-10-31 17:45:15 +0900

    add Recommend

---
+++ components/Top/Recommend/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.scss'
+
+export default function Recommend(): JSX.Element {
+  return (
+    <>
+      <div className={styles.reccomendSec}>
+        <h2>RECOMMEND</h2>
+      </div>
+    </>
+  )
+}
