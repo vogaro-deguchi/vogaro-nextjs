4562baa29398ead2df63feb8b2bd1329a10fc54d
deguchi <deguchi37458@gmail.com>
2022-10-31 17:45:15 +0900

    add Recommend

--- components/Top/index.tsx
+++ components/Top/index.tsx
@@ -1,9 +1,11 @@
 import MV from '@/components/Top/MV'
+import Recommend from '@/components/Top/Recommend'
 
 export default function Top(): JSX.Element {
   return (
     <>
       <MV />
+      <Recommend />
     </>
   )
 }
