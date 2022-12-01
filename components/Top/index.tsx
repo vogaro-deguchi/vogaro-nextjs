ef5f5696b0d2cd2bdbe5282e2eca63e5c474c598
deguchi <deguchi37458@gmail.com>
2022-12-01 19:52:07 +0900

    create SecProduct

--- components/Top/index.tsx
+++ components/Top/index.tsx
@@ -1,11 +1,13 @@
 import MV from '@/components/Top/MV'
 import SecRecommend from '@/components/Top/SecRecommend'
+import SecProduct from '@/components/Top/SecProduct'
 
 export default function Top(): JSX.Element {
   return (
     <>
       <MV />
       <SecRecommend />
+      <SecProduct />
     </>
   )
 }
