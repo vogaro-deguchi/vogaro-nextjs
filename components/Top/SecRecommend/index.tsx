e3846789b2434b14ce659262ab89f1aad761d36f
deguchi <deguchi37458@gmail.com>
2022-12-01 19:45:59 +0900

    create SecTitle

--- components/Top/SecRecommend/index.tsx
+++ components/Top/SecRecommend/index.tsx
@@ -1,18 +1,14 @@
 import styles from './index.module.scss'
 import Container from '@/components/Common/Container'
 import Product from '@/components/Top/Product'
+import SecTitle from '@/components/Top/SecTitle'
 
 export default function Recommend(): JSX.Element {
   return (
     <>
       <div className={styles.recommendSec}>
         <Container>
-          <div className={styles.recommendSec__head}>
-            <h2 className={styles.recommendSec__head__heading}>
-              <span className={styles.main}>RECOMMEND</span>
-              <span className={styles.sub}>おすすめ商品</span>
-            </h2>
-          </div>
+          <SecTitle />
           <div className={styles.productWrapper}>
             <Product />
             <Product />
