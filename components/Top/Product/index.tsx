162317ed2925ae743922f591d43f2b19c845fa47
deguchi <deguchi37458@gmail.com>
2022-12-01 18:50:02 +0900

    update recommend

--- components/Top/Product/index.tsx
+++ components/Top/Product/index.tsx
@@ -14,6 +14,13 @@ export default function Product(): JSX.Element {
         <p className={styles.name}>ルナークスコレクション</p>
         <p className={styles.desc}>冬の限定コフレ登場！月の光をまとう、ワンランク上の美しさへ。</p>
         <p className={styles.price}>6,930円（税込）</p>
+        <div className={styles.tags}>
+          <span className={styles.tag}>限定品</span>
+          <span className={styles.tag}>限定品</span>
+          <span className={styles.tag}>限定品</span>
+          <span className={styles.tag}>限定品</span>
+          <span className={styles.tag}>限定品</span>
+        </div>
       </div>
     </>
   )
