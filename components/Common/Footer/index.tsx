3f9136d2bd749fccbf0f639f9ed239ac09d83cdf
deguchi <deguchi37458@gmail.com>
2022-12-07 12:51:41 +0900

    [update] link

--- components/Common/Footer/index.tsx
+++ components/Common/Footer/index.tsx
@@ -1,5 +1,7 @@
 import styles from './index.module.scss'
 
+import Link from 'next/link'
+
 import Container from '@/components/Common/Container'
 
 export default function Footer() {
@@ -10,39 +12,39 @@ export default function Footer() {
           <div className={styles.culumn}>
             <ul className={styles.menu}>
               <li className={styles.menu__title}>カテゴリから探す</li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><Link href="/skincare">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="/skincare">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="/skincare">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="/skincare">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="/skincare">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="/skincare">スキンケア</Link></li>
             </ul>
             <ul className={styles.menu}>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
             </ul>
           </div>
           <div className={styles.culumn}>
             <ul className={styles.menu}>
               <li className={styles.menu__title}>カテゴリから探す</li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
             </ul>
             <ul className={styles.menu}>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
-              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
+              <li className={styles.menu__item}><Link href="">スキンケア</Link></li>
             </ul>
           </div>
           <div className={styles.culumn}>
