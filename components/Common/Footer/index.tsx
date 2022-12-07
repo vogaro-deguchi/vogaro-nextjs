fdc7af264e017408416336969d73d53425d243b3
deguchi <deguchi37458@gmail.com>
2022-12-07 11:12:26 +0900

    [update] footer

--- components/Common/Footer/index.tsx
+++ components/Common/Footer/index.tsx
@@ -1,9 +1,31 @@
 import styles from './index.module.scss'
 
+import Container from '@/components/Common/Container'
+
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      Footer
+      <Container>
+        <div>
+          <ul className={styles.menu}>
+            <li className={styles.menu__title}>カテゴリから探す</li>
+            <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            <li className={styles.menu__item}><a href="">スキンケア</a></li>
+          </ul>
+          <ul className={styles.menu}>
+            <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            <li className={styles.menu__item}><a href="">スキンケア</a></li>
+          </ul>
+        </div>
+      </Container>
     </footer>
   )
 }
