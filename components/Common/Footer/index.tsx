159b1b416f9dabd229bda0e781d36d5f1d03b6e1
deguchi <deguchi37458@gmail.com>
2022-12-07 11:19:33 +0900

    [update] footer

--- components/Common/Footer/index.tsx
+++ components/Common/Footer/index.tsx
@@ -6,24 +6,54 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <Container>
-        <div>
-          <ul className={styles.menu}>
-            <li className={styles.menu__title}>カテゴリから探す</li>
-            <li className={styles.menu__item}><a href="">スキンケア</a></li>
-            <li className={styles.menu__item}><a href="">スキンケア</a></li>
-            <li className={styles.menu__item}><a href="">スキンケア</a></li>
-            <li className={styles.menu__item}><a href="">スキンケア</a></li>
-            <li className={styles.menu__item}><a href="">スキンケア</a></li>
-            <li className={styles.menu__item}><a href="">スキンケア</a></li>
-          </ul>
-          <ul className={styles.menu}>
-            <li className={styles.menu__item}><a href="">スキンケア</a></li>
-            <li className={styles.menu__item}><a href="">スキンケア</a></li>
-            <li className={styles.menu__item}><a href="">スキンケア</a></li>
-            <li className={styles.menu__item}><a href="">スキンケア</a></li>
-            <li className={styles.menu__item}><a href="">スキンケア</a></li>
-            <li className={styles.menu__item}><a href="">スキンケア</a></li>
-          </ul>
+        <div className={styles.footerContainer}>
+          <div className={styles.culumn}>
+            <ul className={styles.menu}>
+              <li className={styles.menu__title}>カテゴリから探す</li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            </ul>
+            <ul className={styles.menu}>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            </ul>
+          </div>
+          <div className={styles.culumn}>
+            <ul className={styles.menu}>
+              <li className={styles.menu__title}>カテゴリから探す</li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            </ul>
+            <ul className={styles.menu}>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+              <li className={styles.menu__item}><a href="">スキンケア</a></li>
+            </ul>
+          </div>
+          <div className={styles.culumn}>
+
+          </div>
+          <div className={styles.culumn}>
+
+          </div>
+          <div className={styles.culumn}>
+
+          </div>
         </div>
       </Container>
     </footer>
