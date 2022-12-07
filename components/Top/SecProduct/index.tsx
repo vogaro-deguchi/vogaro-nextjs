b10b18b46ad46a690dbdd3f5011da6a6fb408657
deguchi <deguchi37458@gmail.com>
2022-12-07 10:39:45 +0900

    [update] SecProduct

--- components/Top/SecProduct/index.tsx
+++ components/Top/SecProduct/index.tsx
@@ -1,19 +1,62 @@
+import Image from 'next/image'
+
 import styles from './index.module.scss'
+
 import Container from '@/components/Common/Container'
-import Product from '@/components/Top/Product'
+// import Product from '@/components/Top/Product'
 import SecTitle from '@/components/Top/SecTitle'
+
+const categories = [
+
+]
 
 export default function SecProduct(): JSX.Element {
   return (
     <>
-      <div className={styles.recommendSec}>
+      <div className={styles.productSec}>
         <Container>
           <SecTitle
             main = "PRODUCT"
             sub = "商品を探す"
           />
           <div className={styles.productWrapper}>
-            
+            <div className={styles.menu}>
+              <h3 className={styles.menu__title}>カテゴリから探す</h3>
+              <ul className={styles.menu__lists}>
+                <li className={styles.menu__list}>
+                  <a href="" className={styles.menu__link}>
+                    <Image 
+                      src="/assets/Top/SecProduct/example.jpg" width={48} height={48} alt="image"
+                    />
+                    <span>スキンケア</span>
+                  </a>
+                </li>
+                <li className={styles.menu__list}>
+                  <a href="" className={styles.menu__link}>
+                    <Image 
+                      src="/assets/Top/SecProduct/example.jpg" width={48} height={48} alt="image"
+                    />
+                    <span>スキンケア</span>
+                  </a>
+                </li>
+                <li className={styles.menu__list}>
+                  <a href="" className={styles.menu__link}>
+                    <Image 
+                      src="/assets/Top/SecProduct/example.jpg" width={48} height={48} alt="image"
+                    />
+                    <span>スキンケア</span>
+                  </a>
+                </li>
+                <li className={styles.menu__list}>
+                  <a href="" className={styles.menu__link}>
+                    <Image 
+                      src="/assets/Top/SecProduct/example.jpg" width={48} height={48} alt="image"
+                    />
+                    <span>スキンケア</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </Container>
       </div>
