62a6849c1593a670b0ef169a76a8683bffe46431
deguchi <deguchi37458@gmail.com>
2023-02-16 18:52:31 +0900

    [update]

--- lib/constans.tsx
+++ lib/constans.tsx
@@ -1,6 +1,6 @@
 export const siteMeta = {
-  siteTitle: 'Next.js Template',
-  siteDesc: 'Next.jsのテンプレート',
+  siteTitle: 'Vogaro株式会社 - ヴォガロ株式会社',
+  siteDesc: 'Vogaroは“ビジネス課題を解決する”ことをミッションとして、マーケティングとクリエイティブの両軸において、戦略立案から施策実行までを一気通貫で支援する会社です',
   siteUrl: 'http://localhost:3000',
   siteLang: 'ja',
   siteLocale: 'ja_JP',
