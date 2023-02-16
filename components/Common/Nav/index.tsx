fb74002fe3756af0f716600f0efc4536d01842f5
deguchi <deguchi37458@gmail.com>
2023-02-16 17:34:39 +0900

    [update]

--- components/Common/Nav/index.tsx
+++ components/Common/Nav/index.tsx
@@ -1,15 +1,16 @@
+import Link from 'next/link'
+
 import styles from './index.module.scss'
 
 export default function Nav () {
   return (
     <nav className={styles.nav}>
       <ul className={styles.menu}>
-        <li>Who We Are</li>
-        <li>Service</li>
-        <li>Projects</li>
-        <li>News</li>
-        <li>Recruit</li>
-        <li>Contact</li>
+        <li><Link href="about">Who We Are</Link></li>
+        <li><Link href="services">Services</Link></li>
+        <li><Link href="projects">Projects</Link></li>
+        <li><Link href="news">News</Link></li>
+        <li><Link href="contact">Contact</Link></li>
       </ul>
     </nav>
   )
