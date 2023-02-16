7fc8ab5cfa57af81982fb169624cb6c5e5a84b3a
deguchi <h.deguchi@vogaro.co.jp>
2023-02-16 18:52:31 +0900

    [update]

---
+++ components/Services/index.tsx
@@ -0,0 +1,24 @@
+import BreadCrumb from '../Common/BreadCrumb'
+import PageMV from '@/components/Page/PageMV'
+
+export default function  (): JSX.Element {
+  return (
+    <>
+      <BreadCrumb
+        lists={[
+          {
+            name: 'ホーム',
+            path: '/'
+          },
+          {
+            name: 'スキンケア'
+          }
+        ]}
+      />
+      <PageMV
+        main = "Services"
+        sub = "サービス紹介"
+      />
+    </>
+  )
+}
--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from 'next'
 
 import Meta from '@/components/Common/Meta'
-import About from '@/components/About/index'
+import Services from '@/components/Services/index'
 
 const Home: NextPage = () => {
   return (
     <>
       <Meta 
-        pageTitle = "Who We Are"
+        pageTitle = "Services"
       />
-      <About />
+      <Services />
     </>
   )
 }
