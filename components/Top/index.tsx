34cebf9eb150f7694fabd2e4776276b01610c78e
deguchi <h.deguchi@vogaro.co.jp>
2023-02-16 16:58:47 +0900

    [update]

--- components/Top/index.tsx
+++ components/Top/index.tsx
@@ -1,11 +1,13 @@
 import MV from '@/components/Top/MV'
 import SecWhoweare from '@/components/Top/SecWhoweare'
+import SecService from '@/components/Top/SecService'
 
 export default function Top (): JSX.Element {
   return (
     <>
       <MV />
       <SecWhoweare />
+      <SecService />
     </>
   )
 }
