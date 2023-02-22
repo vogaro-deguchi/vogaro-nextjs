ab2f6c6fa9164f5beb1435224cf9300982909746
deguchi <deguchi37458@gmail.com>
2023-02-22 15:08:19 +0900

    [update] PAgeMv

--- components/Page/PageMV/index.tsx
+++ components/Page/PageMV/index.tsx
@@ -3,7 +3,8 @@ import Nav from '@/components/Common/Nav'
 import Image from 'next/image'
 import styles from './index.module.scss'
 
-import logo from './assets/mv_01.png'
+import bg from './assets/mv_01.png'
+import logo from './assets/pagemv_logo.png'
 
 export default function PageMV (props:any): JSX.Element {
   return (
@@ -12,7 +13,7 @@ export default function PageMV (props:any): JSX.Element {
         {/* <Swiper /> */}
         <Nav></Nav>
         <Image
-          src={logo}
+          src={bg}
           alt=""
           width="100%"
           height="100%"
@@ -24,6 +25,16 @@ export default function PageMV (props:any): JSX.Element {
         <div className={styles.pageTitle}>
           <h2 className={styles.main}>{props.main}</h2>
           <p className={styles.sub}>{props.sub}</p>
+        </div>
+        <div className={styles.pageLogo}>
+          <Image
+            src={logo}
+            alt=""
+            objectFit="cover"
+            layout='responsive'
+            priority
+            placeholder='blur'
+          />
         </div>
       </div>
     </>
