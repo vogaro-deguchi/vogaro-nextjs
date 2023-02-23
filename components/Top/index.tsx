ab13e83fe1a075468020797df02bc13c5df384be
deguchi <deguchi37458@gmail.com>
2023-02-23 21:51:05 +0900

    [remode] remove MV comp

--- components/Top/index.tsx
+++ components/Top/index.tsx
@@ -1,4 +1,8 @@
-import MV from '@/components/Top/MV'
+import Nav from '@/components/Common/Nav'
+import Image from 'next/image'
+
+import styles from './index.module.scss'
+
 import SecWhoweare from '@/components/Top/SecWhoweare'
 import SecService from '@/components/Top/SecService'
 import SecProject from '@/components/Top/SecProject'
@@ -6,7 +10,28 @@ import SecProject from '@/components/Top/SecProject'
 export default function Top (): JSX.Element {
   return (
     <>
-      <MV />
+      <div className={styles.secMv}>
+        {/* <Swiper /> */}
+        <Nav></Nav>
+        <Image
+          src='/assets/Top/mv_bg.png'
+          alt=""
+          width="100%"
+          height="100%"
+          objectFit="cover"
+          layout='fill'
+          priority
+        />
+        <div className={styles.secMv__logo}>
+          <Image
+            src='/assets/Top/mv_logo.png'
+            alt=""
+            width="300"
+            height="388"
+            priority
+          />
+        </div>
+      </div>
       <SecWhoweare />
       <SecService />
       <SecProject />
